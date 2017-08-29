002f6e4043fd032e9269c5baabd420d1961c50f6
Mylan Connolly <mylan.connolly@protonmail.com>
2017-08-29 13:27:11 -0400

    Initial commit

---
+++ lib/pewpew.ex
@@ -0,0 +1,18 @@
+defmodule PewPew do
+  @moduledoc """
+  Documentation for PewPew.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PewPew.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pewpew_test.exs
@@ -0,0 +1,8 @@
+defmodule PewPewTest do
+  use ExUnit.Case
+  doctest PewPew
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
